cab0c4ea9dfff8df2dbff96a64ffc93909ba964a
Ascriipt <maparigi@student.42.fr>
2022-08-29 18:16:30 +0200

    init struct s_philo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 12:44:12 by maparigi          #+#    #+#             */
-/*   Updated: 2022/08/27 17:41:01 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/08/29 18:15:20 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*threadtest(void *id)
 {
 	pthread_mutex_lock(&mutex_test);
 	printf("I am thread %d\n", *(int *)id);
-	usleep(1000000);
+	usleep(100000);
 	pthread_mutex_unlock(&mutex_test);
 	return (id);
 }
@@ -38,23 +38,23 @@ void	att_val(int ac, char **av, t_arg *args)
 
 int	main(int argc, char **argv)
 {
-	pthread_t	*philos;
+	t_philo		*philos;
 	t_arg		args;
 	int			i;
 
 	if (argc < 5 || argc > 6)
 		return (1);
 	att_val(argc, argv, &args);
-	philos = malloc(sizeof(philos) * args.nop);
+	philos = malloc(sizeof(t_philo) * args.nop);
 	if (!philos)
 		return (2);
 	pthread_mutex_init(&mutex_test, NULL);
 	i = -1;
 	while (++i < args.nop)
 	{
-		if (pthread_create(&philos[i], NULL, &threadtest, &i) != 0)
+		if (pthread_create(&(philos[i].philo_id), NULL, &threadtest, &i) != 0)
 			return (3);
-		if (pthread_join(philos[i], NULL) != 0)
+		if (pthread_join(philos[i].philo_id, NULL) != 0)
 			return (4);
 	}
 	pthread_mutex_destroy(&mutex_test);
